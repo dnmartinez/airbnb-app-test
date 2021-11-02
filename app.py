3de90583dc974dc25b11991a3f9cb479b50690ca
Naomi Martinez <dnmartinez94@gmail.com>
2021-11-02 08:42:25 -0500

    secrets manager configuration

--- app.py
+++ app.py
@@ -1,17 +1,11 @@
 from flask import Flask, request, render_template, jsonify, make_response
+from secrets_manager import get_secret
 import pymysql
 import pymysql.cursors
+import boto3
 
-from secrets_manager import get_secret
 
-"""db_username = "admin"
-db_password = "yokocat22"
-db_name = "listings"
-db_endpoint = "naomi-airbnb-db.chjxclsv9tv3.us-east-2.rds.amazonaws.com""""
-client = boto3.client('secretsmanager')
-response = client.get_secret_value(
-    SecretId='naomi-airbnb-db-sm'
-)
+
 app = Flask(__name__)
 db_config = get_secret()
 
@@ -26,11 +20,11 @@ def affordabe():
     connection= pymysql.connect(host=db_config["host"], user=db_config["username"], passwd=db_config["password"], db=db_config["db_name"], cursorclass=pymysql.cursors.DictCursor)
     with connection:
         with connection.cursor() as cursor:
-            # Read a single record
             sql = "SELECT * FROM nl_listing WHERE price <= 150 ORDER BY price"
             cursor.execute(sql)
             result = cursor.fetchall()
     return make_response(jsonify({'lisnting': result}))
+    
     #return render_template("affordable.html", data=result)
         
         #return render_template("affordable.html", name=result)
